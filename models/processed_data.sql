b4a84fb67ab0a63987730b737a75a83af7d73278
StevenSeeger <sseeger1996@gmail.com>
2023-01-20 10:51:19 -0700

    moving comma

--- models/processed_data.sql
+++ models/processed_data.sql
@@ -10,8 +10,8 @@ WITH renamed AS (
             key,
             value
         FROM raw_data, jsonb_each_text(raw_data._airbyte_data)
-        ORDER BY 1, 2',
-        }}
+        ORDER BY 1, 2'
+        }},
         $$VALUES ('host'), ('title'), ('yields'), ('total_time'), ('ingredients'), ('instructions')$$
     ) AS ct ("_airbyte_ab_id" text, "host" text, "title" text, "yields" text, "total_time" text, "ingredients" text, "instructions" text)
 )
