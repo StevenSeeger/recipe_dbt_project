9a8fb39a2c0ddc520c9d64d142cf7b64adc9d8de
StevenSeeger <sseeger1996@gmail.com>
2023-01-20 10:39:17 -0700

    remove semicolon

--- models/processed_data.sql
+++ models/processed_data.sql
@@ -11,7 +11,7 @@ renamed AS (
         FROM raw_data, jsonb_each_text(raw_data._airbyte_data)
         ORDER BY 1, 2',
         $$VALUES ('host'), ('title'), ('yields'), ('total_time'), ('ingredients'), ('instructions')$$
-    ) AS ct ("_airbyte_ab_id" text, "host" text, "title" text, "yields" text, "total_time" text, "ingredients" text, "instructions" text);
+    ) AS ct ("_airbyte_ab_id" text, "host" text, "title" text, "yields" text, "total_time" text, "ingredients" text, "instructions" text)
 )
 SELECT *
 FROM renamed
