c06419804d12bc8b395cbc6e48e6891f752827c4
StevenSeeger <sseeger1996@gmail.com>
2023-01-20 10:55:12 -0700

    something new

--- models/processed_data.sql
+++ models/processed_data.sql
@@ -1,17 +1,15 @@
 WITH renamed AS (
     SELECT * 
     FROM crosstab(
-        {{
         'with raw_data AS (
-            SELECT * FROM ' ~ source('public', '_airbyte_raw_test') ~ '
+            SELECT * FROM ' {{ source('public', '_airbyte_raw_test') }} '
         )
         SELECT 
             _airbyte_ab_id,
             key,
             value
         FROM raw_data, jsonb_each_text(raw_data._airbyte_data)
-        ORDER BY 1, 2'
-        }},
+        ORDER BY 1, 2',
         $$VALUES ('host'), ('title'), ('yields'), ('total_time'), ('ingredients'), ('instructions')$$
     ) AS ct ("_airbyte_ab_id" text, "host" text, "title" text, "yields" text, "total_time" text, "ingredients" text, "instructions" text)
 )
